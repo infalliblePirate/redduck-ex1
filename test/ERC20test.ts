5c5c58d3cc84446e2e0f902015d78cb9f2505dfb
infalliblePirate <pavlenko.kateryna@lll.kpi.ua>
2025-10-11 15:01:42 +0200

    feat: add transfer & transferFrom tests for ERC20.

--- test/ERC20test.ts
+++ test/ERC20test.ts
@@ -58,7 +58,7 @@ describe('ERC20 test', () => {
             await expect(
                 token.approve(hre.ethers.ZeroAddress, expectedApprovedBalance)
             ).to.be.revertedWith("The receipient is a zero address");
-        })
+        });
 
         it('should return true and update allowance', async () => {
             const { deployer, user, token } = await setup();
@@ -74,5 +74,63 @@ describe('ERC20 test', () => {
         });
     });
 
+    describe('Transfer', () => {
+        it('should revert', async () => {
+            const { deployer, user, token } = await setup();
+            const zeroSigner = await hre.ethers.getSigner(hre.ethers.ZeroAddress);
 
+            await expect(token.connect(zeroSigner).getFunction('transfer')
+                .staticCall(user, expectedApprovedBalance)).to.be.revertedWith("The sender is a zero address");
+
+            await expect(
+                token.transfer(hre.ethers.ZeroAddress, expectedApprovedBalance)
+            ).to.be.revertedWith("The receipient is a zero address");
+
+            await expect(token.connect(await hre.ethers.getSigner(user)).transfer(deployer, expectedApprovedBalance)
+            ).to.be.revertedWith("The transferable value exceeds balance");
+        });
+
+        it('should return true, update user\'s balances and emit Transfer event', async () => {
+            const { deployer, user, token } = await setup();
+
+            expect(await token.getFunction('transfer')
+                .staticCall(user, expectedApprovedBalance)).to.eq(true);
+
+            await expect(token.transfer(user, expectedApprovedBalance)).emit(token, 'Transfer')
+                .withArgs(deployer, user, expectedApprovedBalance);
+
+            const senderBalance = await token.balanceOf(deployer);
+            const receipientBalance = await token.balanceOf(user);
+
+            expect(senderBalance).to.eq(expectedSupply - expectedApprovedBalance);
+            expect(receipientBalance).to.eq(expectedApprovedBalance);
+        });
+    });
+
+    describe('TransferFrom', () => {
+        it('should revert', async () => {
+            const { deployer, user, token } = await setup();
+            await expect(token.transferFrom(deployer, user, expectedApprovedBalance))
+                .to.be.rejectedWith('The transferable value exceeds allowance');
+        });
+
+        it('should return true, update user\'s balances, allowances and emit Transfer, Approval events', async () => {
+            const { deployer, user, token } = await setup();
+            await expect(token.approve(user, expectedApprovedBalance)).emit(token, 'Approval')
+                .withArgs(deployer, user, expectedApprovedBalance);
+
+            expect(await token.connect(await hre.ethers.getSigner(user)).getFunction('transferFrom')
+                .staticCall(deployer, user, expectedApprovedBalance,)).to.eq(true);
+
+            await expect(token.connect(await hre.ethers.getSigner(user)).transferFrom(deployer, user, expectedApprovedBalance))
+                .emit(token, 'Approval')
+                .withArgs(deployer, user, 0)
+                .emit(token, 'Transfer')
+                .withArgs(deployer, user, expectedApprovedBalance);
+
+            expect(await token.allowance(deployer, user)).to.eq(0);
+            expect(await token.balanceOf(deployer)).to.eq(expectedSupply - expectedApprovedBalance);
+            expect(await token.balanceOf(user)).to.eq(expectedApprovedBalance);
+        });
+    });
 });
