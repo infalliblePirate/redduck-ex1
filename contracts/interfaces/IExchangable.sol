247be6884df0c94930a44a439f5f6cb09ecb2b6a
infalliblePirate <pavlenko.kateryna@lll.kpi.ua>
2025-10-12 16:39:05 +0200

    feat: add IExchangable interface.

---
+++ contracts/interfaces/IExchangable.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+interface IExchangable {
+    event Buy(
+        address buyer,
+        uint256 tokensBought,
+        uint256 spentEth,
+        uint256 feeInTokens
+    );
+    event Sell(
+        address seller,
+        uint256 tokensSold,
+        uint256 sentEth,
+        uint256 feeInTokens
+    );
+
+    event LiquidityChanged(
+        address initiator,
+        uint256 ethAmount,
+        uint256 tokenAmount
+    );
+
+    function buy() external payable returns (bool);
+
+    function sell(uint256 value) external returns (bool);
+
+    function addLiquidity(uint256 tokenSupply) external payable returns (bool);
+
+    function liquidity() external view returns (uint256, uint256);
+
+    function price() external view returns (uint256);
+
+    function setPrice(uint256) external returns (bool);
+
+    function fee() external returns(uint8);
+
+    function setFee(uint8) external returns (bool);
+}
