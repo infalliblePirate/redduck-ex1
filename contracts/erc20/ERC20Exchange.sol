0048ce498f1eada02db0191e324ea3863fbccf84
infalliblePirate <pavlenko.kateryna@lll.kpi.ua>
2025-10-13 01:06:11 +0200

    feat: change constructor to accept token & add getters, setters.

--- contracts/erc20/ERC20Exchange.sol
+++ contracts/erc20/ERC20Exchange.sol
@@ -1,26 +1,67 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "../interfaces/IExchangable.sol";
 import "../erc20/ERC20.sol";
 
-contract ERC20Exchange {
-
+contract ERC20Exchange is IExchangable, Ownable {
     ERC20 internal _token;
 
-    uint256 internal _price;          // in wei
-    uint8 internal _percetageFee;     /// NOT SET 
-    uint256 internal _accumulatedFee; // in ERC20 tokens
+    uint256 internal _price;
+    uint8 internal _percetageFee; 
+    uint256 internal _accumulatedFee;
+    uint16 private constant FEE_DENOMINATOR = 10_000;
+
+    uint256 internal _ethReserve;
+
     constructor(
-        uint8 decimals,
-        string memory name,
-        string memory symbol,
-        uint256 initialSupply,
-        uint256 price
-    ) {
-        _token = new ERC20(decimals, name, symbol, initialSupply);
-        _price = price * 1 ether;
-        uint256 supply = initialSupply * 10 ** decimals;
-        _token.mint(address(this), supply);
+        address erc20,
+        uint256 price_,
+        uint8 percentageFee
+    ) Ownable(msg.sender) {
+        require(erc20 != address(0), "The token is a zero address");
+        require(price_ > 0, "The price must be a positive number");
+        require(
+            percentageFee > 0 && percentageFee < FEE_DENOMINATOR,
+            "The fee is out of range"
+        );
+        _token = ERC20(erc20);
+        _price = price_;
     }
 
+    function addLiquidity(
+        uint256 tokenSupply
+    ) external payable override onlyOwner returns (bool) {
+    }
+
+    function price() external view override returns (uint256) {
+        return _price;
+    }
+
+    function token() external view returns (address) {
+        return address(_token);
+    }
+
+    function setPrice(uint256 price_) external override returns (bool) {
+        _price = price_;
+        return true;
+    }
+
+    function buy() external payable override returns (bool) {
+        
+    }
+
+    function sell(uint256 value) external override returns (bool) {
+        
+    }
+
+    function liquidity() external view override returns (uint256, uint256) {
+        return (_ethReserve, _token.balanceOf(address(this)));
+    }
+
+    function fee() external override returns (uint8) {}
+
+    function setFee(uint8) external override returns (bool) {}
 }
