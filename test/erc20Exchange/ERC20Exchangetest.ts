b1b5de366217a9a49947855111c6c71bfdc11c6b
infalliblePirate <pavlenko.kateryna@lll.kpi.ua>
2025-10-13 16:41:52 +0200

    test(erc20exchange): add liquidity test.

--- test/erc20Exchange/ERC20Exchangetest.ts
+++ test/erc20Exchange/ERC20Exchangetest.ts
@@ -10,7 +10,8 @@ describe('ERC20Exchange test', () => {
     const decimals = 6;
     const expectedSupply = hre.ethers.parseUnits("1000", decimals);
     const expectedPrice = hre.ethers.parseEther("0.000001");
-    const expectedAddedLiquidity = hre.ethers.parseEther("1");
+    const expectedAddedLiquidityEth = hre.ethers.parseEther("1");
+    const expectedFee = 10;
 
     const setup = async (): Promise<ERC20ExchangeSetup> => {
         const [deployer, user] = await hre.ethers.getSigners();
@@ -19,10 +20,10 @@ describe('ERC20Exchange test', () => {
             .deploy(decimals, name, symbol);
 
         const exchange = await new ERC20Exchange__factory(deployer).deploy(
-            token, expectedPrice);
+            token, expectedPrice, expectedFee);
 
         await token.setMinter(exchange);
-        await exchange.addLiquidity(expectedSupply, { value: expectedAddedLiquidity });
+        await exchange.addLiquidity(expectedSupply, { value: expectedAddedLiquidityEth });
 
         return {
             deployer,
@@ -37,6 +38,47 @@ describe('ERC20Exchange test', () => {
             const { exchange, token } = await setup();
             expect(await exchange.price()).to.eq(expectedPrice);
             expect(await exchange.token()).to.eq(token);
+        });
+    });
+
+    describe('Add liquidity', () => {
+        it('should revert', async () => {
+            const { user, exchange } = await setup();
+
+            await expect(exchange.connect(user).addLiquidity(0, { value: expectedAddedLiquidityEth }))
+                .to.be.reverted;
+
+            await expect(exchange.addLiquidity(0, { value: expectedAddedLiquidityEth }))
+                .to.be.revertedWith("The inital supply must be a positive number");
+
+            await expect(exchange.addLiquidity(expectedSupply, { value: 0 }))
+                .to.be.revertedWith("The ether reserve must be a positive number");
+        });
+
+        it('should update liquidity, balances, emit LiquidityChanged', async () => {
+            const { deployer, exchange } = await setup();
+
+            const deployerEthBefore = await hre.ethers.provider.getBalance(deployer);
+            const [exchangeLiquidityEthBefore, exchangeLiquidityTokenBefore] = await exchange.liquidity();
+
+            const tx = await exchange.addLiquidity(expectedSupply, { value: expectedAddedLiquidityEth });
+            await expect(tx)
+                .to.emit(exchange, 'LiquidityChanged')
+                .withArgs(deployer, expectedAddedLiquidityEth, expectedSupply);
+
+            const receipt = (await tx.wait())!;
+            const gasCost = receipt.gasUsed * tx.gasPrice;
+
+            const deployerEthAfter = await hre.ethers.provider.getBalance(deployer);
+            const [exchangeLiquidityEthAfter, exchangeLiquidityTokenAfter] = await exchange.liquidity();
+
+            expect(deployerEthBefore - deployerEthAfter)
+                .to.be.closeTo(expectedAddedLiquidityEth + gasCost, 1e12);
+            expect(exchangeLiquidityEthAfter - exchangeLiquidityEthBefore)
+                .to.eq(expectedAddedLiquidityEth);
+
+            expect(exchangeLiquidityTokenAfter - exchangeLiquidityTokenBefore)
+                .to.eq(expectedSupply);
         });
     });
 });
