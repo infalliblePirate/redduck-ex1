f50cb9079d6e79812976bded59009f4d11166329
infalliblePirate <pavlenko.kateryna@lll.kpi.ua>
2025-10-14 12:02:41 +0200

    test(erc20exchange): add test for buy function.

--- test/erc20Exchange/ERC20Exchangetest.ts
+++ test/erc20Exchange/ERC20Exchangetest.ts
@@ -5,13 +5,20 @@ import { ERC20__factory } from "../../typechain-types";
 import { ERC20ExchangeSetup } from "./types";
 
 describe('ERC20Exchange test', () => {
+    const FEE_DENOMINATOR: bigint = 10_000n;
+
     const name = 'Penguin';
     const symbol = 'PNGN';
     const decimals = 6;
+
     const expectedSupply = hre.ethers.parseUnits("1000", decimals);
     const expectedPrice = hre.ethers.parseEther("0.000001");
     const expectedAddedLiquidityEth = hre.ethers.parseEther("1");
-    const expectedFee = 10;
+    const expectedFeeBasisPoints: bigint = 10n;
+    
+    const tradeAmount = hre.ethers.parseEther("0.000001");
+    const insufficientEthAmount = hre.ethers.parseUnits("0.1", decimals);
+    const excessiveTradeEthAmount = expectedSupply * expectedPrice + 1n;
 
     const setup = async (): Promise<ERC20ExchangeSetup> => {
         const [deployer, user] = await hre.ethers.getSigners();
@@ -20,7 +27,7 @@ describe('ERC20Exchange test', () => {
             .deploy(decimals, name, symbol);
 
         const exchange = await new ERC20Exchange__factory(deployer).deploy(
-            token, expectedPrice, expectedFee);
+            token, expectedPrice, expectedFeeBasisPoints);
 
         await token.setMinter(exchange);
         await exchange.addLiquidity(expectedSupply, { value: expectedAddedLiquidityEth });
@@ -79,6 +86,52 @@ describe('ERC20Exchange test', () => {
 
             expect(exchangeLiquidityTokenAfter - exchangeLiquidityTokenBefore)
                 .to.eq(expectedSupply);
+        });
+    });
+
+    describe('Buy', () => {
+        it('should update liquidity, user\'s eth and token balance, return true, emit Buy event', async () => {
+            const { user, exchange, token } = await setup();
+
+            const userEthBefore = await hre.ethers.provider.getBalance(user);
+            const [exchangeEthBefore, exchangeTokenBefore] = await exchange.liquidity();
+            const userTokensBefore = await token.balanceOf(user);
+
+            expect(await exchange.getFunction('buy').staticCall({value: expectedPrice}))
+                .to.eq(true);
+
+            const tx = await exchange.connect(user).buy({ value: expectedPrice });
+            const receipt = (await tx.wait())!;
+
+            const gasUsed = receipt.gasUsed;
+            const gasPrice = tx.gasPrice;
+            const gasCost = gasUsed * gasPrice;
+
+            const userEthAfter = await hre.ethers.provider.getBalance(user);
+            const [exchangeEthAfter, exchangeTokenAfter] = await exchange.liquidity();
+            const userTokenAfter = await token.balanceOf(user);
+
+            expect(userEthBefore - expectedPrice).to.be.closeTo(userEthAfter + gasCost, 12n);
+            expect(exchangeEthAfter - expectedPrice).to.eq(exchangeEthBefore);
+
+            const tokens = tradeAmount * 10n ** await token.decimals() / expectedPrice;
+            const fee = tokens * expectedFeeBasisPoints / FEE_DENOMINATOR;
+            const tokensAfterFee = tokens - fee;
+
+            expect(userTokenAfter).to.eq(userTokensBefore + tokensAfterFee);
+            expect(await exchange.accumulatedFee()).to.eq(fee);
+            expect(exchangeTokenBefore - exchangeTokenAfter).to.eq(tokensAfterFee);
+
+            await expect(tx).to.emit(exchange, 'Buy')
+                .withArgs(user, tokensAfterFee, tradeAmount, fee);
+        });
+        it('should revert', async () => {
+            const { user, exchange } = await setup();
+            await expect(exchange.connect(user).buy({ value: insufficientEthAmount }))
+                .to.be.revertedWith("No sufficient funds to buy token");
+
+            await expect(exchange.connect(user).buy({ value: excessiveTradeEthAmount }))
+                .to.be.revertedWith("The number of requested tokens exceeds liquidity pool");
         });
     });
 });
